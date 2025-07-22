302fa313eccafe9a310bbea0d6a0d86cfb10c050
pengfei liu <liu.pengfei@hotmail.fr>
2025-07-22 15:14:39 +0200

    add script for extra routes duration spark job

--- scripts/french_commune_distance/calculate_distance_duration_for_spec_commune.py
+++ scripts/french_commune_distance/calculate_distance_duration_for_spec_commune.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from typing import Tuple
 
 from pyspark.sql import SparkSession, DataFrame
 import requests
@@ -40,6 +41,15 @@ def get_osrm_host(env_key, default: str = "127.0.0.1:5000") -> str:
 
 def get_route(lat_start: str, long_start: str, lat_end: str, long_end: str,
               show_steps: str = "false") -> dict:
+    """
+    This function calls the orsm rest api to get possible routes in car drive mode
+    :param lat_start: latitude of the starting point
+    :param long_start: longitude of the starting point
+    :param lat_end: latitude of the ending point
+    :param long_end: longitude of the ending point
+    :param show_steps: flag to indicate whether to show steps of the routes or not
+    :return:
+    """
     host = get_osrm_host(ENV_KEY)
     start_point = f"{long_start},{lat_start}"
     end_point = f"{long_end},{lat_end}"
@@ -58,18 +68,23 @@ def get_route(lat_start: str, long_start: str, lat_end: str, long_end: str,
     return json_response
 
 
-def parse_route_json(input_route: dict) -> (float, float):
-    route = input_route['routes'][0]
-    if route:
-        # the raw distance is in meter
-        distance = route["distance"]
-        # the raw duration is in second
-        # the returned duration is in minutes
-        duration = round((route["duration"] / 60), 2)
-    else:
-        distance = 0
-        duration = 0
-    return distance, duration
+def parse_route_json(input_route: dict) -> Tuple[float, float]:
+    """
+    This function parse the route result json that is returned by the orsm api.
+    :param input_route:
+    :return: a tuple (distance, duration)
+    """
+    try:
+        route = input_route.get('routes', [])[0]
+        if not route:
+            return 0.0, 0.0
+
+        distance = float(route.get("distance", 0))
+        duration = round(float(route.get("duration", 0)) / 60, 2)
+        return distance, duration
+
+    except (IndexError, TypeError, ValueError, KeyError):
+        return 0.0, 0.0
 
 
 def calculate_distance_duration(lat_start: str, long_start: str, lat_end: str, long_end: str) -> (float, float):
@@ -87,59 +102,43 @@ def get_distance_duration(lat_start: str, long_start: str, lat_end: str, long_en
     return calculate_distance_duration_str(lat_start, long_start, lat_end, long_end)
 
 
-def calculate_distance_duration_matrix_in_patch(insee_code_list: list, centroid_df: DataFrame, output_file_path: str,
-                                                patch_size: int = 4, partition_num: int = 4):
-    # split the input insee code list into patch
-    for i in range(0, len(insee_code_list), patch_size):
-        patch_code_list = insee_code_list[i:i + patch_size]
-        # 1. build the source commune df with the given insee code
-        commune_df = centroid_df.filter(col("insee").isin(patch_code_list))
-        # 2. build a matrix with given source commnue which joins all others commune
-        commune_matrix_df = commune_df.alias("add1").join(centroid_df.alias("add2"),
-                                                          col("add1.insee") != col("add2.insee"), "inner").select(
-            col("add1.longitude").alias("source_long"), col("add1.latitude").alias("source_lat"),
-            col("add1.insee").alias("source_insee"), col("add1.nom").alias("source_nom"),
-            col("add2.longitude").alias("dest_long"), col("add2.latitude").alias("dest_lat"),
-            col("add2.insee").alias("dest_insee"), col("add2.nom").alias("dest_nom"))
-        commune_matrix_df = commune_matrix_df.repartition(partition_num)
-        # 3. calculate the distance and duration
-        distance_duration_df = commune_matrix_df.withColumn("distance_duration",
-                                                            get_distance_duration(col("source_lat"), col("source_long"),
-                                                                                  col("dest_lat"),
-                                                                                  col("dest_long"))).select(
-            "source_nom", "source_insee", "dest_nom", "dest_insee", "distance_duration").withColumn("distance(meter)",
-                                                                                                    split(
-                                                                                                        col("distance_duration"),
-                                                                                                        ";")[
-                                                                                                        0]).withColumn(
-            "duration(minutes)", split(col("distance_duration"), ";")[1]).drop("distance_duration")
-        # 4. write the result into a parquet file
-        distance_duration_df.write.mode("append").partitionBy("source_insee").parquet(output_file_path)
+def calculate_distance_duration_matrix_in_patch(route_matrix_df: DataFrame, output_file_path: str):
+    """
+    This function calculate the distance and duration between src and dest commune in the giving matrix
+    :param route_matrix_df:
+    :param output_file_path:
+    :param patch_size:
+    :param partition_num:
+    :return:
+    """
+    # 3. calculate the distance and duration
+    distance_duration_df = route_matrix_df.withColumn("distance_duration",
+                                                        get_distance_duration(col("source_lat"), col("source_long"),
+                                                                              col("dest_lat"),
+                                                                              col("dest_long"))).select(
+        "source_nom", "source_insee", "dest_nom", "dest_insee", "distance_duration").withColumn("distance(meter)",
+                                                                                                split(
+                                                                                                    col("distance_duration"),
+                                                                                                    ";")[
+                                                                                                    0]).withColumn(
+        "duration(minutes)", split(col("distance_duration"), ";")[1]).drop("distance_duration")
+    # 4. write the result into a parquet file
+    distance_duration_df.write.mode("append").partitionBy("source_insee").parquet(output_file_path)
 
 
 def main():
     # get argument from command line
     if len(sys.argv) != 3:
-        print("Usage: python calculate_distance_duration.py <osrm_host> <partition_number>")
+        print("Usage: python calculate_distance_duration.py <osrm_host>")
         return
 
     # step1: set osrm host
     osrm_host = str(sys.argv[1])
     os.environ[ENV_KEY] = osrm_host
-    partition = int(sys.argv[2])
-
-    # input argument config
-    # path of the code list split files parent dir
-    code_list_parent_dir = "/tmp/code_split_test"
-    #
-    start_part = 0
-    end_part = 0
-    # output result file path
-    output_file_path = "/tmp/duration_test"
 
     # step2: Create a SparkSession
     spark = SparkSession.builder \
-        .appName("Example PySpark Job") \
+        .appName("Extra route duration calculation") \
         .getOrCreate()
 
     # step3: read the converted french commune centroid parquet file
